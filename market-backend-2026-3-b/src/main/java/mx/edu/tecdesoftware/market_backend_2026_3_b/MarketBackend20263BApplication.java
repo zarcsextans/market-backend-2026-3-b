690bac9432969d478ca502d75be73524ef20c0da
ScarletRuelas <Zarcsextans@gmail.com>
2026-05-11 11:30:45 -0600

    chore: initial Spring Initial Boot project setup

---
+++ market-backend-2026-3-b/src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_b/MarketBackend20263BApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.tecdesoftware.market_backend_2026_3_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketBackend20263BApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketBackend20263BApplication.class, args);
+	}
+
+}
---
+++ market-backend-2026-3-b/src/test/java/mx/edu/tecdesoftware/market_backend_2026_3_b/MarketBackend20263BApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.tecdesoftware.market_backend_2026_3_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketBackend20263BApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
