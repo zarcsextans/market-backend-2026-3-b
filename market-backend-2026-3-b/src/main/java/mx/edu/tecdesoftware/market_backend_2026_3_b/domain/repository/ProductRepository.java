6e9d5c7765282c6a2c5370b94d34840cf2763ff2
ScarletRuelas <Zarcsextans@gmail.com>
2026-06-09 11:33:56 -0600

    feat: add domain models and configure MapStruct for entity mapping Implemented Spring Data Query Methods (findByCategoria, getEscasos). Added domain models (Category, Product) and ProductRepository interface. Configured MapStruct dependency and IntelliJ plugin. Created CategoryMapper and ProductMapper to translate between entities and domain objects.

--- market-backend-2026-3-b/src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_b/domain/repository/ProductRepository.java
+++ market-backend-2026-3-b/src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_b/domain/repository/ProductRepository.java
@@ -5,13 +5,13 @@ import mx.edu.tecdesoftware.market_backend_2026_3_b.domain.Product;
 import java.util.List;
 import java.util.Optional;
 
-public interface ProductRepository {
+public interface ProductRepository  {
 
     List<Product> getAll();
 
     Optional<List<Product>> getByCategory(int categoryId);
 
-    Optional<List<Product>> getScarseProducts(int quantity);
+    Optional<List<Product>>  getScarseProducts(int quantity);
 
     Optional<Product> getProduct(int productId);
 
