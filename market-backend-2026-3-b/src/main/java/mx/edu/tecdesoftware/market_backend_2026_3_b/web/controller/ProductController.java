016de52599e921790252c76b351dd00943de289b
ScarletRuelas <Zarcsextans@gmail.com>
2026-07-07 11:27:41 -0600

    feat(web): finalize ProductController wit CRUD endpoints

--- market-backend-2026-3-b/src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_b/web/controller/ProductController.java
+++ market-backend-2026-3-b/src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_b/web/controller/ProductController.java
@@ -1,10 +1,13 @@
 package mx.edu.tecdesoftware.market_backend_2026_3_b.web.controller;
 
-import mx.edu.tecdesoftware.market_backend_2026_3_b.domain.repository.ProductRepository;
+import mx.edu.tecdesoftware.market_backend_2026_3_b.domain.Product;
 import mx.edu.tecdesoftware.market_backend_2026_3_b.domain.service.ProductService;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -12,7 +15,60 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
-    @Autowired
-    private ProductRepository productRepository;
+
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAll() {
+
+        return ResponseEntity.ok(productService.getAll());
+
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(
+            @PathVariable("id") int productId) {
+
+        return productService.getProduct(productId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+
+    }
+
+
+    @GetMapping("/category/{categoryId}")
+    public ResponseEntity<List<Product>> getByCategory(
+            @PathVariable("categoryId") int categoryId) {
+
+        return productService.getByCategory(categoryId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Product> save(
+            @RequestBody Product product) {
+
+        return ResponseEntity.ok(productService.save(product));
+
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(
+            @PathVariable("id") int productId) {
+
+
+        if(productService.delete(productId)) {
+
+            return ResponseEntity.ok().build();
+
+        }
+
+        return ResponseEntity.notFound().build();
+
+    }
 
 }
